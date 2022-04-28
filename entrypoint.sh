dc3c9169233f609dad526072123d96bb3b7b7ea7
Brad Bishop <bradleyb@fuzziesquirrel.com>
2022-04-27 20:07:49 -0400

    entrypoint.sh: run shfmt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,13 @@
 set -eu
 
 # validate that none of the required environment variables are empty
-if [ -z "${HOSTS}" ]
-then
-  echo "ERROR: Missing one or more of the following variables"
-  echo "  HOSTS"
-  exit 1
+if [ -z "${HOSTS}" ]; then
+	echo "ERROR: Missing one or more of the following variables"
+	echo "  HOSTS"
+	exit 1
 fi
 
-cat > /usr/bin/smokeping_prober <<EOF
+cat >/usr/bin/smokeping_prober <<EOF
 #!/bin/sh
 exec /usr/lib/smokeping_prober/smokeping_prober ${ARGS:-} ${HOSTS}
 EOF
